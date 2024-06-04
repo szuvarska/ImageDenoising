382a497b161e505585b3069b0349713a19e324f3
szuvarska <01171269@pw.edu.pl>
2024-06-04 19:48:44 +0200

    Improve gifs

--- denoise.py
+++ denoise.py
@@ -7,23 +7,27 @@ import imageio.v2 as imageio
 import os
 
 
-def save_frame(avg: np.array, directory_name: str = 'image', i: int = 1):
+def save_frame(avg: np.array, digits, directory_name: str = 'image', i: int = 1):
     avg[avg >= 0] = 1
     avg[avg < 0] = -1
     avg = avg.astype(np.int32)
     plt.imshow(avg, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1)
+    plt.axis('off')  # This line removes the axes
+    plt.gca().set_position([0, 0, 1, 1])  # This line makes the figure fill the whole plot
     directory_path = f"frames/{directory_name}"
     os.mkdir(directory_path) if not os.path.exists(directory_path) else None
-    plt.savefig(f"{directory_path}/frame_{i}.png")
+    plt.savefig(f"{directory_path}/frame_{str(i).zfill(digits)}.png", bbox_inches='tight', pad_inches=0,
+                transparent=True)
+    plt.clf()  # This line clears the current figure
 
 
-def create_gif(output_file_name: str, input_directory_name: str = 'image', frame_duration: float = 0.1):
+def create_gif(output_file_name: str, input_directory_name: str = 'image', fps: int = 10):
     images = []
     for file_name in sorted(os.listdir(f"frames/{input_directory_name}")):
         if file_name.endswith('.png'):
             file_path = os.path.join(f"frames/{input_directory_name}", file_name)
             images.append(imageio.imread(file_path))
-    imageio.mimsave(f"gifs/{output_file_name}.gif", images, format='GIF', duration=frame_duration)
+    imageio.mimsave(f"gifs/{output_file_name}.gif", images, fps=fps)
 
 
 def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 500000, invtemp: float = 2.0,
@@ -36,19 +40,22 @@ def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 50
     for _ in range(burnin):
         gg.gibbs_move()
 
+    digits = len(str(loops))
     # Sample
     avg = np.zeros_like(noisy).astype(np.float64)
     for i in range(loops):
         gg.gibbs_move()
         avg += gg.grid
-        if i % (loops / save_frames_iter) == 0:
-            save_frame(avg / (i + 1), title, i + 1)
+        # use the commented line below if you want to make the beginning slower
+        # if (i < loops / 500 and i % (loops / 5000) == 0) or (i > loops / 500 and i % (loops / save_frames_iter) == 0):
+        if i % (loops/save_frames_iter) == 0:
+            save_frame(avg / (i + 1), digits, title, i + 1)
 
     return avg / loops
 
 
 def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: float = 0.9, burnin: int = 50000,
-            loops: int = 500000, invtemp: float = 2.0, title="denoise", save_frames_iter: int = 100):
+            loops: int = 500000, invtemp: float = 2.0, title="denoise", save_frames_iter: int = 100, fps: int = 10):
     image = skimage.io.imread(file_path)
     image = (image[:, :, 0].astype(np.int32) * 2) - 1
     noise = np.random.random(size=image.size).reshape(image.shape) > noise_strength
@@ -62,13 +69,14 @@ def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: floa
     fig, axes = plt.subplots(ncols=2, figsize=(11, 6))
     axes[0].imshow(avg, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1)
     axes[1].imshow(noisy, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1)
-    create_gif(title, title)
+    create_gif(title, title, fps)
 
 
 def main():
-    # denoise("img/mini_logo.png", noise_strength=0.9, extfield_strength=0.9, burnin=50000, loops=500000, title="mini")
-    # plt.show()
-    create_gif("mini_slower", "mini", 5)
+    denoise("img/mini_logo.png", noise_strength=0.9, extfield_strength=0.9, burnin=50000, loops=500000,
+            title="mini",
+            fps=3)
+    plt.show()
 
 
 if __name__ == "__main__":
