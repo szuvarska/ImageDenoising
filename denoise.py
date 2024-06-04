4eecac3c6fe5f5759d4a771893fe3dc83bd7bdbf
nizwant <mateusznizwantowski@gmail.com>
2024-06-04 19:14:38 +0200

    titles and axis removed in denoise and formatting

--- denoise.py
+++ denoise.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 500000, invtemp: float = 2.0):
+def isingdenoise(
+    noisy: np.array,
+    q: float,
+    burnin: int = 50000,
+    loops: int = 500000,
+    invtemp: float = 2.0,
+):
     h = 0.5 * np.log(q / (1 - q))
     gg = IsingGridVaryingField(noisy.shape[0], noisy.shape[1], h * noisy, invtemp)
     gg.grid = np.array(noisy)
@@ -22,8 +28,14 @@ def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 50
     return avg / loops
 
 
-def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: float = 0.9, burnin: int = 50000,
-            loops: int = 500000, invtemp: float = 2.0):
+def denoise(
+    file_path: str,
+    noise_strength: float = 0.9,
+    extfield_strength: float = 0.9,
+    burnin: int = 50000,
+    loops: int = 500000,
+    invtemp: float = 2.0,
+):
     image = skimage.io.imread(file_path)
     image = (image[:, :, 0].astype(np.int32) * 2) - 1
     noise = np.random.random(size=image.size).reshape(image.shape) > noise_strength
@@ -35,12 +47,28 @@ def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: floa
     avg = avg.astype(np.int32)
 
     fig, axes = plt.subplots(ncols=2, figsize=(11, 6))
-    axes[0].imshow(avg, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1)
-    axes[1].imshow(noisy, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1)
+    axes[0].imshow(
+        avg, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1
+    )
+    axes[0].set_title("Denoised")
+    axes[1].imshow(
+        noisy, cmap=cm.gray, aspect="equal", interpolation="none", vmin=-1, vmax=1
+    )
+    axes[1].set_title("Noisy with q = {:.2f}".format(noise_strength))
+    for ax in axes:
+        # remove the y-axis
+        ax.yaxis.set_visible(False)
+        ax.xaxis.set_visible(False)
 
 
 def main():
-    denoise("img/mini_logo.png", noise_strength=0.9, extfield_strength=0.9, burnin=50000, loops=500000)
+    denoise(
+        "img/mini_logo.png",
+        noise_strength=0.9,
+        extfield_strength=0.9,
+        burnin=50000,
+        loops=500000,
+    )
     plt.show()
 
 
