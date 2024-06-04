88bffe3bae5f5895c1e2c88431db5ec046f9e288
szuvarska <01171269@pw.edu.pl>
2024-06-04 19:55:35 +0200

    Make gifs optional and remove frames after creating gif

--- denoise.py
+++ denoise.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import imageio.v2 as imageio
 import os
+import shutil
 
 
 def save_frame(avg: np.array, digits, directory_name: str = 'image', i: int = 1):
@@ -28,10 +29,12 @@ def create_gif(output_file_name: str, input_directory_name: str = 'image', fps: 
             file_path = os.path.join(f"frames/{input_directory_name}", file_name)
             images.append(imageio.imread(file_path))
     imageio.mimsave(f"gifs/{output_file_name}.gif", images, fps=fps)
+    # Delete the directory with the frames
+    shutil.rmtree(f"frames/{input_directory_name}")
 
 
 def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 500000, invtemp: float = 2.0,
-                 title: str = "denoise", save_frames_iter: int = 100):
+                 make_gif: bool = False, title: str = "denoise", save_frames_iter: int = 100):
     h = 0.5 * np.log(q / (1 - q))
     gg = IsingGridVaryingField(noisy.shape[0], noisy.shape[1], h * noisy, invtemp)
     gg.grid = np.array(noisy)
@@ -46,22 +49,25 @@ def isingdenoise(noisy: np.array, q: float, burnin: int = 50000, loops: int = 50
     for i in range(loops):
         gg.gibbs_move()
         avg += gg.grid
+        if not make_gif:
+            continue
         # use the commented line below if you want to make the beginning slower
         # if (i < loops / 500 and i % (loops / 5000) == 0) or (i > loops / 500 and i % (loops / save_frames_iter) == 0):
-        if i % (loops/save_frames_iter) == 0:
+        if i % (loops / save_frames_iter) == 0:
             save_frame(avg / (i + 1), digits, title, i + 1)
 
     return avg / loops
 
 
 def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: float = 0.9, burnin: int = 50000,
-            loops: int = 500000, invtemp: float = 2.0, title="denoise", save_frames_iter: int = 100, fps: int = 10):
+            loops: int = 500000, invtemp: float = 2.0, make_gif: bool = False, title: str = "denoise",
+            save_frames_iter: int = 100, fps: int = 10):
     image = skimage.io.imread(file_path)
     image = (image[:, :, 0].astype(np.int32) * 2) - 1
     noise = np.random.random(size=image.size).reshape(image.shape) > noise_strength
     noisy = np.array(image)
     noisy[noise] = -noisy[noise]
-    avg = isingdenoise(noisy, extfield_strength, burnin, loops, invtemp, title, save_frames_iter)
+    avg = isingdenoise(noisy, extfield_strength, burnin, loops, invtemp, make_gif, title, save_frames_iter)
     avg[avg >= 0] = 1
     avg[avg < 0] = -1
     avg = avg.astype(np.int32)
@@ -74,8 +80,7 @@ def denoise(file_path: str, noise_strength: float = 0.9, extfield_strength: floa
 
 def main():
     denoise("img/mini_logo.png", noise_strength=0.9, extfield_strength=0.9, burnin=50000, loops=500000,
-            title="mini",
-            fps=3)
+            make_gif=True, title="mini", fps=3)
     plt.show()
 
 
